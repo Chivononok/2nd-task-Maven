bf32709c6ae9416b9a4088514d582673db6bee50
Chivononok <sergey.v.kononovich@gmail.com>
2025-09-13 16:35:23 +0300

    Изменил значение параметра

--- core/src/main/java/corepackage/Main.java
+++ core/src/main/java/corepackage/Main.java
@@ -5,6 +5,6 @@ import apipackage.TestApi;
 public class Main {
     public static void main(String[] args){
         TestApi ta = new TestApi();
-        System.out.println(ta.helloTestApi("Сережп - молодец!!!"));
+        System.out.println(ta.helloTestApi("Сережа - молодец!!!"));
     }
 }
